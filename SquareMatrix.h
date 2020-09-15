3deb6246655cf22163be0406d8581ff8e781c6a3
Stephanie <stephanieb@tywls-astoria.org>
2020-09-15 16:46:58 -0400

    the skeleton of my SquareMatrix.h file

---
+++ SquareMatrix.h
@@ -0,0 +1,30 @@
+#pragma once
+
+template<typename T>
+
+class SquareMatrix
+{
+private:
+    /* data */
+public:
+    SquareMatrix(); //default constructor
+
+    //Big Five
+    ~SquareMatrix(); //deconstructo 
+    SquareMatrix(const SquareMatrix& trg); //copy constructor
+    SquareMatrix(SquareMatrix&& trg); //move constructor
+    SquareMatrix& operator=(const SquareMatrix& rhs); //copy assignment
+    SquareMatrix& operator=(SquareMatrix&& rhs); //move assignment
+
+};
+
+SquareMatrix::SquareMatrix(/* args */)
+{
+}
+
+SquareMatrix::~SquareMatrix()
+{
+}
+
+
+//Due October 5
