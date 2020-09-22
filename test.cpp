80378453705467bb0c9d6ab744d4e0c340a3d6cd
Stephanie <stephanieb@tywls-astoria.org>
2020-09-21 23:27:22 -0400

    fixed errors

--- test.cpp
+++ test.cpp
@@ -8,8 +8,8 @@ void Test1()
     SquareMatrix<int> matrix_a, matrix_b;
     cout<< "Matrix A Size: "<< matrix_a.size()<< endl;
     cout<< "Matrix B Size: "<< matrix_a.size()<< endl;
-    SquareMatrix<int> matrix_c[10];
-    cout<<matrix_c.size()
+    SquareMatrix<int> matrix_c{10};
+    cout<<matrix_c.size();
 
 }
 int main()
