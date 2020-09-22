9a371d9a5d1b8dbfb3a44a098bbdc424cdbe6829
Stephanie <stephanieb@tywls-astoria.org>
2020-09-21 23:14:31 -0400

    fixed errors

--- test.cpp
+++ test.cpp
@@ -5,26 +5,9 @@ using namespace std;
 
 void Test1()
 {
-   /* 
-   SquareMatrix<int> matrix_a, matrix_b;
+    SquareMatrix<int> matrix_a[10], matrix_b[10];
     cout<< "Matrix A Size: "<< matrix_a._size;<< endl;
     cout<< "Matrix B Size: "<< matrix_a._size;<< endl;
-
-    SquareMatrix<int> matrix_c{1};
-    cout<<"Matrix C" << matrix_c <<endl;
-    matrix_a.resize(6);
-    cout<< "Matrix A Size: "<< matrix_a._size;<< endl;
-    SquareMatrix<int> matrix_d{matrix_a}; 
-    cout<<"Matrix D" << matrix_d <<endl;
-    SquareMatrix<int> matrix_e = move(matrix_d); 
-    cout<<"Matrix E" << matrix_e <<endl;
-    matrix_b = move(e);
-    cout<<"Matrix B" << matrix_b <<endl;
-    cout<<"Matrix E" << matrix_e <<endl; 
-    */
-
-
-
 
 }
 int main()
