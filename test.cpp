d00ad7598973f45cc083a609a2522ae59e814afc
Stephanie <stephanieb@tywls-astoria.org>
2020-09-21 23:19:53 -0400

    fixed errors

--- test.cpp
+++ test.cpp
@@ -6,8 +6,8 @@ using namespace std;
 void Test1()
 {
     SquareMatrix<int> matrix_a, matrix_b;
-    cout<< "Matrix A Size: "<< matrix_a.size();<< endl;
-    cout<< "Matrix B Size: "<< matrix_a.size();<< endl;
+    cout<< "Matrix A Size: "<< matrix_a.size()<< endl;
+    cout<< "Matrix B Size: "<< matrix_a.size()<< endl;
 
 }
 int main()
